5b5bcadaac480609ccc5eccb614337b6590c38f2
cbonaga-stn <chel.bonaga@gmail.com>
2025-11-20 21:48:05 +0800

    Mongoose trial

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect()
+  .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
   .then(() => {
     app.listen(5005);
   })
