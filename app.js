3e019be47754abf5acdc831c58015fb0c63b7f4e
cbonaga-stn <chel.bonaga@gmail.com>
2025-11-20 21:50:26 +0800

    FIx

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+  .connect(process.env.DB_URL)
   .then(() => {
     app.listen(5005);
   })
